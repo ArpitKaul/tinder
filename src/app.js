e2264bc77e675a38b63771c20707864db551e5c4
ArpitKaul <akaul7407@gmail.com>
2025-04-11 21:56:12 +0530

    cerated routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require('express'); 
-
 const app = express();
 
-app.use("/tes ",(req , res)=>{
-    res.send("hello from the server!")
+app.use("/user", 
+    (req , res)=>{
+     console.log("Handling the route user!!");
+     res.send("Response!!"); 
+},
+(req , res)=>{
+    console.log("Handling the route  2!!");
+    res.send("2nd Response!!");
 })
 
-app.listen(7777 , ()=> {
+app.listen(7777, () => {
     console.log('Server started on port 7777');
 });
- 
+   
 
+// Multiple Route handlers - play with the code
+// next 
+// next function and errors along with res.send()
+// app.use("/route" , rh [rh2 rh3], rh4 , rh5);
